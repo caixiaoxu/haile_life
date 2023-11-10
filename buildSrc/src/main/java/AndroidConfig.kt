273ee54eb2244c3e99aee4a5e637bcc4b0460202
Lsy <1228357237@qq.com>
2023-11-10 09:56:43 +0800

    预约订单提交时不验证设备

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -10,7 +10,7 @@ object AndroidConfig {
         val applicationId = "com.yunshang.haile_life"
         val minSdk = 21
         val targetSdk = 33
-        val versionCode = 10104
-        val versionName = "1.1.4"
+        val versionCode = 10105
+        val versionName = "1.1.5"
     }
 }
