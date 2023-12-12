efe8aaeef0497e9584319028d9477ec3ad0755f7
Lsy <1228357237@qq.com>
2023-12-12 14:54:55 +0800

    筒自洁预计时间刷新

--- app/src/main/java/com/yunshang/haile_life/data/entities/SelfCleanRefreshEntity.kt
+++ app/src/main/java/com/yunshang/haile_life/data/entities/SelfCleanRefreshEntity.kt
@@ -1,5 +1,8 @@
 package com.yunshang.haile_life.data.entities
 
+import com.yunshang.haile_life.utils.DateTimeUtils
+import kotlin.math.ceil
+
 /**
  * Title :
  * Author: Lsy
@@ -14,6 +17,21 @@ data class SelfCleanRefreshEntity(
     val status: Int = 0, //设备执行状态。<40 执行中，40 执行完成，50 执行失败
     val finishTime: String? = null // 预计结束时间.格式：yyyy-MM-dd HH:mm:ss
 ) {
-    val selfCleanStateVal:String
-    get() = if (status < 40) "清洁中" else "筒自洁结束"
+    val selfCleanStateVal: String
+        get() = if (status < 40) "清洁中" else "筒自洁结束"
+
+    //<40 执行中，40 执行完成，50 执行失败.
+    fun selfPrompt(remain: String?): String =
+        if (status < 40) {
+            var minute = ceil(
+                ((DateTimeUtils.formatDateFromString(finishTime)?.time
+                    ?: 0) - System.currentTimeMillis()) / 1000 / 60.0
+            ).toInt()
+            if (minute < 1) {
+                minute = 1
+            }
+            "筒自洁清洁中，预计耗时${minute}分钟"
+        } else if (40 == status) {
+            "筒自洁已结束"
+        } else "选择筒自洁，享受${remain}分钟免费筒自洁"
 }
